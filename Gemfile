d983d790788f9f8fe9bca94e7770e9e0091f8f63
keiminamin <ktaro0512@docomo.ne.jp>
2020-06-12 07:57:32 +0000

    second

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'rake'
 gem 'sinatra-activerecord'
-gem 'activerecord','5.2.3'
+gem 'activerecord','6.0.3.1gi'
 gem 'bcrypt'
 group :development  do
 
--- models.rb
+++ models.rb
@@ -1,9 +1,11 @@
+require 'bundler/setup'
+Bundler.require
+
+
+if development?
 
 
 ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
 
-
-class User < ActiveRecord::Base
-  has_secure_password
 
 end
